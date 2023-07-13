9fd35a91c3b640a02ff808540a138350c13d9d7c
Utavegu <utavegu@list.ru>
2023-07-13 11:48:46 +0500

    build: add mongoose and config connect to db in app-module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://${process.env.MONGODB_SERVICE_NAME}:${
+        process.env.MONGODB_INTERNAL_PORT || 27017
+      }`,
+      {
+        user: process.env.MONGODB_LOGIN || 'root',
+        pass: process.env.MONGODB_PASSWORD || 'example',
+        dbName: process.env.DB_NAME,
+      },
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
