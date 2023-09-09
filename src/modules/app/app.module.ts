2c1320dc74cd655e2dc99e272415ffc513ed8c1b
Utavegu <utavegu@list.ru>
2023-09-09 17:34:00 +0500

    chore: configured mailer, connected mail-module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '../users/users.module';
 import { AuthModule } from '../auth/auth.module';
+import { MailModule } from '../mail/mail.module';
+
+// TODOs ниже
 
 @Module({
   imports: [
     MongooseModule.forRoot(
+      // И тут, пожалуй, куда-то в другое место бы
       `mongodb://${process.env.MONGODB_SERVICE_NAME}:${
         process.env.MONGODB_INTERNAL_PORT || 27017
       }`,
@@ -22,8 +27,22 @@ import { AuthModule } from '../auth/auth.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'test'),
     }),
+    // TODO: А это точно именно тут должно быть или можно в модуле почты? Поизучай ещё получше настройки nestjs-mailer-а
+    MailerModule.forRoot({
+      transport: {
+        // этот объект в какой-нибудь сетап-файл, пожалуй, лучше.
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT) || 0,
+        secure: true, // Ну, по хорошему-то тру
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+    }),
     UsersModule,
     AuthModule,
+    MailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MailModule,
   ],
   providers: [UsersService],
   controllers: [UsersController], // TODO: не забудь удалить отсюда
