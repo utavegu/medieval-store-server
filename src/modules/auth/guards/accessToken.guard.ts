9f1152c34c9dcb49b6eae54503e208f6e441ca3e
Utavegu <utavegu@list.ru>
2023-09-15 14:49:19 +0500

    refactor: access-token-guard

--- src/modules/auth/guards/accessToken.guard.ts
+++ src/modules/auth/guards/accessToken.guard.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class AccessTokenGuard extends AuthGuard('jwt') {
+  /*
   // TODO: внутри фигурных скобок изначально ничего не было, и возможно то, что я тут нагородил ради кастомной ошибки для интерсептора фронтенда сломает логику (в том числе и доставания юзера на фронтенде) - следи, тести...
   public canActivate(context: ExecutionContext) {
     return super.canActivate(context);
@@ -15,6 +16,7 @@ export class AccessTokenGuard extends AuthGuard('jwt') {
   public handleRequest(err: any, user: any, info: any) {
     if (info) {
       // TODO: Попробуй поизящнее... Ещё и асинк-эвэйты забыл, кстати. Операции с джейсоном асинхронны. Ппц как извращенно выглядит, надо почитать про это info и как с ним работать.
+      // TODO: Всё, на фронтенде сделал иначе - через 401 м возможности аксиоса. Но оставлю это тут, так как интересный опыт. И с инфо, всё-таки, желательно разобраться. Но для "чистового варианта" можно вернуть изначальную версию файла - ВЕРНУЛ
       const infoString = JSON.stringify(info);
       const infoJson = JSON.parse(infoString);
       throw new UnauthorizedException(infoJson.name);
@@ -27,6 +29,7 @@ export class AccessTokenGuard extends AuthGuard('jwt') {
     //     ERROR_MESSAGES.ONLY_AVAILABLE_AUTHENTICATED_USERS,
     //   );
     // }
-    return user; // TODO: переделай
+    return user; // TODO: переделай, юзера-то тут никакого нет сейчас
   }
+  */
 }
